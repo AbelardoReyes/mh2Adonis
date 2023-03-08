117527bb9b032275f3d3e85718e5dcc6c22f1c9c
Abelardo <abelardoreyes256@gmail.com>
2023-03-08 01:21:40 -0600

    Acomodo de rutas

--- start/routes.ts
+++ start/routes.ts
@@ -25,12 +25,12 @@ Route.post('/usuario/crear', 'UsersController.registrarUsuario')
 Route.post('/usuario/login', 'UsersController.login')
 Route.post('/usuario/recuperarCuenta', 'UsersController.recuperarCuenta')
 Route.post('/usuario/cambiarPassword', 'UsersController.cambiarPassword')
-Route.get('/usuario/info', 'UsersController.infoUser').middleware('auth')
 Route.post('/usuario/logout', 'UsersController.logout').middleware('auth')
-Route.get('/usuario', 'UsersController.infoUsuario').middleware('auth')
+
+
 
 Route.get('/usuario/infoObjeto', 'UsersController.infoUserObjeto').middleware('auth')
-Route.get('/usuario/info/:id', 'UsersController.infoIDUser').middleware('auth').where('id', '[0-9]+')
+
 Route.put('/usuario/update/:id', 'UsersController.updateUser').middleware('auth').where('id', '[0-9]+')
 Route.put('/usuario/updateRole/:id', 'UsersController.soloRol').middleware('auth').where('id', '[0-9]+')
 Route.put('/usuario/desactivar/:id', 'UsersController.desactivar').middleware('auth').where('id', '[0-9]+')
@@ -39,21 +39,30 @@ Route.get('/usuario/validarToken', 'UsersController.validarToken').middleware('a
 Route.get('/verify/:id', 'UsersController.verify').as('verify')
 Route.post('/codigo/:id', 'UsersController.codigo').as('codigo')
 
-Route.post('/chef', 'ChefsController.registrarChef').middleware('checkRole:1')
-Route.get('/chef', 'ChefsController.obtenerChefs').middleware('activo')
-Route.get('/chef/:id', 'ChefsController.obtenerChef')
-Route.put('/chef/:id', 'ChefsController.actualizarChef')
-Route.delete('/chef/:id', 'ChefsController.eliminarChef')
 
-Route.post('/receta', 'RecetasController.registrarReceta')
-Route.get('/receta', 'RecetasController.obtenerRecetas')
-Route.get('/receta/:id', 'RecetasController.obtenerReceta')
-Route.put('/receta/:id', 'RecetasController.actualizarReceta')
-Route.delete('/receta/:id', 'RecetasController.eliminarReceta')
+Route.group(() => {
+  //Usuarios
+  Route.get('/usuario/info/:id', 'UsersController.infoIDUser').middleware('auth').where('id', '[0-9]+')
+  Route.get('/usuario', 'UsersController.infoUsuario').middleware('auth')
+  Route.get('/usuario/info', 'UsersController.infoUser').middleware('auth')
+  Route.post('/chef', 'ChefsController.registrarChef').middleware('checkRole:1')
+  //Chef
+  Route.get('/chef', 'ChefsController.obtenerChefs')
+  Route.get('/chef/:id', 'ChefsController.obtenerChef')
+  Route.put('/chef/:id', 'ChefsController.actualizarChef')
+  Route.delete('/chef/:id', 'ChefsController.eliminarChef')
+  //Recetas
+  Route.post('/receta', 'RecetasController.registrarReceta')
+  Route.get('/receta', 'RecetasController.obtenerRecetas')
+  Route.get('/receta/:id', 'RecetasController.obtenerReceta')
+  Route.put('/receta/:id', 'RecetasController.actualizarReceta')
+  Route.delete('/receta/:id', 'RecetasController.eliminarReceta')
+  //ingredientes
+  Route.post('/ingrediente', 'IngredientesController.registrarIngrediente')
+  Route.get('/ingrediente', 'IngredientesController.obtenerIngredientes')
+  Route.get('/ingrediente/:id', 'IngredientesController.obtenerIngrediente')
+  Route.put('/ingrediente/:id', 'IngredientesController.actualizarIngrediente')
+  Route.delete('/ingrediente/:id', 'IngredientesController.eliminarIngrediente')
 
-Route.post('/ingrediente', 'IngredientesController.registrarIngrediente')
-Route.get('/ingrediente', 'IngredientesController.obtenerIngredientes')
-Route.get('/ingrediente/:id', 'IngredientesController.obtenerIngrediente')
-Route.put('/ingrediente/:id', 'IngredientesController.actualizarIngrediente')
-Route.delete('/ingrediente/:id', 'IngredientesController.eliminarIngrediente')
+}).middleware(['activo', 'auth'])
 
